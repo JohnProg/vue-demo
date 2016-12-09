690323d08bad934a5c906fdae58959fb9e9fbda0
Roger Xu <roger.xu.2007@gmail.com>
2016-12-09 19:04:09 +0800

    Add transitions

--- src/views/App.vue
+++ src/views/App.vue
@@ -9,7 +9,12 @@
         <a>Todo</a>
       </router-link>
     </ul>
-    <router-view></router-view>
+
+    <transition name="fade" mode="out-in">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
   </div>
 </template>
 
@@ -17,3 +22,13 @@
 export default {
 };
 </script>
+
+<style lang="css">
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s ease;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0;
+}
+</style>
